6045600d8e2cc8f8a5bc26f802dba326bca218c0
HCS <helder.costa@eduvaud.ch>
2023-08-27 12:56:50 +0200

    Ajout d'une nouvelle fonctionnalité pour intégrer les commentaires depuis le fichier remarks.md J'ai mis en place une nouvelle fonction pour intégrer les commentaires depuis le fichier remarks.md. Cependant, je suis au regret de vous informer que cette fonctionnalité n'est pas encore opérationnelle.

--- srcSlideIQ/SlideIQ.py
+++ srcSlideIQ/SlideIQ.py
@@ -24,12 +24,35 @@ html_files = [f for f in all_slide_files if f.endswith('.html')]
 if html_files:
     # Construire le chemin complet du premier fichier HTML trouvé
     path_presentation = os.path.join(slide_directory, html_files[0])
+    print(path_presentation)
 else:
     print("Aucun fichier HTML trouvé dans le sous-répertoire 'Slides'.")
     exit(1)  # Quitte le script
 
 path_control = os.path.join(current_directory,"control_window.html")
 print(path_control)
+
+path_remarks = os.path.join(current_directory, "remarks.md")
+print(path_remarks)
+
+def read_remarks_for_slide(file_path):
+    # Read the content of the remarks.md file
+    with open(file_path, 'r') as file:
+        content = file.read()
+
+    # Split the content into slides
+    slides = content.split('## slide')
+
+    # Create a dictionary to store remarks for each slide
+    remarks_dict = {}
+
+    for slide_index, slide_content in enumerate(slides):
+        if slide_index > 0:
+            remarks = slide_content.strip().split('\n- ')
+            remarks_dict[slide_index] = remarks[1:]  # Exclude the first empty line
+
+    return remarks_dict
+    
 
 def create_windows(path_presentation, path_control):
     window_2 = webview.create_window('Master', url=path_control, width=800, height=600)
@@ -38,33 +61,38 @@ def create_windows(path_presentation, path_control):
     return window_1, window_2
 
 def expose_functions(window_1, window_2):
-
     def send_to_goToSlide_window(slide):
         window_1.evaluate_js(f"goToSlide({slide});")
+    window_2.expose(send_to_goToSlide_window)
 
     def send_to_slide_window(message):
         window_1.evaluate_js(f"messageReceived('{message}')")
+    window_2.expose(send_to_slide_window)
 
     def send_to_master_window(message):
         window_2.evaluate_js(f"messageReceived('{message}')")
-
     window_1.expose(send_to_master_window)
-    window_2.expose(send_to_slide_window)
-
-    # Vos fonctions actuelles ici
-    def simulateKeyPress(key: str):
-        window_1.evaluate_js(f"simulateKeyPress({key});")
 
     def currentSlide():
         return window_1.evaluate_js("getCurrentSlideContent();")
+    window_2.expose(currentSlide)
 
     def nextSlide():
         return window_1.evaluate_js("getNextSlideContent();")
-
-    window_2.expose(simulateKeyPress)
-    window_2.expose(currentSlide)
     window_2.expose(nextSlide)
-    window_2.expose(send_to_goToSlide_window)
+
+    def simulateKeyPress(key: str):
+        window_1.evaluate_js(f"simulateKeyPress({key});")
+    window_2.expose(simulateKeyPress)
+
+    def getCurrentIndex():
+        window_1.evaluate_js("getCurrentIndex();")
+    window_2.expose(getCurrentIndex)
+    
+    def get_remarks():
+        remarks_dict = read_remarks_for_slide(path_remarks)  # Obtiens le dictionnaire de remarques
+        return remarks_dict
+    window_2.expose(get_remarks)
 
 def open_windows():
     window_1, window_2 = create_windows(path_presentation,path_control)
--- srcSlideIQ/control_window.js
+++ srcSlideIQ/control_window.js
@@ -1,11 +1,45 @@
 // Attendez que le DOM soit complètement chargé avant d'exécuter le script
 window.addEventListener('DOMContentLoaded', async () => {
     // Récupération des éléments HTML
+
+    // Les boutons pour activer les slide avant ou après 
     const prevButton = document.getElementById('prevButton');
     const nextButton = document.getElementById('nextButton');
+
+    // les captures des slides actuelles et suivantes 
     const currentView = document.getElementById('currentView');
     const nextView = document.getElementById('nextView');
+
+    // permet d'accèder directement à la slide souhaitée 
     const goToSlideButton = document.getElementById('goToSlideButton');
+
+    // permet de rédiger les commentaires pour l'enseignant 
+    const remarksNow = document.getElementById('remarksNow');
+    const remarksNext = document.getElementById('remarksNext');
+
+    // Fonction pour obtenir les remarques du slide souhaité
+    async function getRemarksForSlide(slideIndex) {
+        try {
+            var remarksDict = await window.pywebview.api.get_remarks();  // Accède à la variable exposée depuis Python
+            var remarks = remarksDict[slideIndex];  // Remarques pour le slide souhaité
+            return remarks;
+        } catch (error) {
+            console.error("Erreur lors de la récupération des remarques :", error);
+            return [];  // Retourne une liste vide en cas d'erreur
+        }
+    }
+
+    // Obtient les remarques et met à jour le contenu HTML
+    async function updateRemarks() {
+        var currentSlideIndex = await window.pywebview.api.getCurrentIndex();
+        var nextSlideIndex = currentSlideIndex + 1;
+
+        var remarksNowContent = await getRemarksForSlide(currentSlideIndex);
+        var remarksNextContent = await getRemarksForSlide(nextSlideIndex);
+
+        remarksNow.innerHTML = '<p>' + remarksNowContent + '</p>';
+        remarksNext.innerHTML = '<p>' + remarksNextContent + '</p>';
+    }
 
     // Fonction pour simuler une pression de touche
     async function call_simulateKeyPress(keyCode) {
@@ -13,6 +47,9 @@ window.addEventListener('DOMContentLoaded', async () => {
             await window.pywebview.api.simulateKeyPress(keyCode);
             // Après simulation, mettre à jour le contenu des iframes
             await Promise.all([call_getCurrentSlideContent(), call_getNextSlideContent()]);
+             
+            // Appelle la fonction d'actualisation des remarques
+            updateRemarks();
         } catch (error) {
             console.error("Erreur lors de la simulation de la touche :", error);
         }
@@ -51,7 +88,7 @@ window.addEventListener('DOMContentLoaded', async () => {
         }
     }
 
-    // Bouton test pour envoyer un message à la fenêtre du slide
+    // Bouton pour envoyer un message à la fenêtre du slide
     document.getElementById('SendHTMLButton').addEventListener('click', () => {
         var msg = document.getElementById('inputMsg').value;
         window.pywebview.api.send_to_slide_window();
